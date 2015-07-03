23c4521899c3d89fffb17d70d0d80e5c10fea4d0
houlei <641037548@qq.com>
2015-07-03 15:44:02 +0800

    侯垒添加项目

---
+++ GitTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by 侯垒 on 15/7/3.
+//  Copyright (c) 2015年 侯垒. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by 侯垒 on 15/7/3.
+//  Copyright (c) 2015年 侯垒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
